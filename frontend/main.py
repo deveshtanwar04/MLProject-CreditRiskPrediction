637d5ebeb53119f8f3b3682691ca946e2e285719
deveshtanwar04 <deveshtanwar2official@gmail.com>
2026-07-12 15:21:13 +0530

    Adding API URL

--- frontend/main.py
+++ frontend/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import time
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://credit-risk-pred-engine-api.onrender.com/predict"
 
 # ---------- PAGE CONFIG ----------
 # Sets the browser tab title and widens the layout
